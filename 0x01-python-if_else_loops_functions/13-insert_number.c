3cffc7f608874903702680514e489db523e8d284
Robin60 <owinorobin05@gmail.com>
2023-01-04 14:48:26 +0000

    feat: python if/else functionss

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: the linked list
+ * @number: the value
+ * Return: address of new node, NULL if failed
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function that prints a string in uppercase followed by a new line"""
+uppercase = __import__('8-uppercase').uppercase
+
+uppercase("best")
+uppercase("Best School 98 Battery Street")
+
