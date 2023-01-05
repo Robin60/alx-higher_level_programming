e03c44505ca2345a364aeac404fef189c0096931
Robin60 <owinorobin05@gmail.com>
2023-01-05 10:14:42 +0000

    fix: Adjust ouptup

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,11 +4,14 @@
 
 def fizzbuzz():
     for number in range(0, 101):
-        if (number % 3 == 0 & number % 5 == 0):
-            print("FizzBuzz", end=" ")
-        elif(number % 3 == 0):
-            print("Fizz", end=" ")
-        elif(number % 5 == 0):
-            print("Buzz", end=" ")
-        else:
+        if (number == 0):
             print("{}".format(number), end=" ")
+        else:
+            if (number % 3 == 0 and number % 5 == 0):
+                print("FizzBuzz", end=" ")
+            elif (number % 3 == 0):
+                print("Fizz", end=" ")
+            elif (number % 5 == 0):
+                print("Buzz", end=" ")
+            else:
+                print("{}".format(number), end=" ")
