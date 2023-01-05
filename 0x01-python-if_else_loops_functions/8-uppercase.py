46488abddc78360d02af10ef2b36bec436263f03
Robin60 <owinorobin05@gmail.com>
2023-01-05 07:01:32 +0000

    fix: print to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """function that prints a string in uppercase followed by a new line"""
-uppercase = __import__('8-uppercase').uppercase
 
-uppercase("best")
-uppercase("Best School 98 Battery Street")
 
+def uppercase(str):
+    print(str.upper())
