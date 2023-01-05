051f64bfd7027ab22b53359d3ff657445d7d96a6
Robin60 <owinorobin05@gmail.com>
2023-01-05 12:19:13 +0000

    feat: print ASCII alphabet in reverse

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase (z in lowercase and Y in uppercase)"""
+"""prints the ASCII alphabet, in reverse order, alterna lowercase, uppercase"""
 
 
 i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0;
+    i = 32 if i == 0 else 0
