99b7b18cb703668cb9cc1d0c9309fab78512128e
Robin Owino <owinorobin05@gmail.com>
2023-01-12 06:49:26 -0800

    feat: python more data structures

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,8 +2,8 @@
 
 
 def simple_delete(a_dictionary, key=""):
-    dict = a_dictionary.copy()
-    for k in a_dict.keys():
+    dicts = a_dictionary.copy()
+    for k in a_dicts.keys():
         if k == key:
             del a_dictionary[key]
     return a_dictionary
