b9bce4d1a68a438523873b6a9f43269052ffceae
Robin Owino <owinorobin05@gmail.com>
2023-01-12 06:44:52 -0800

    feat: python more data structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,5 @@
 
 
 def search_replace(my_list, search, replace):
-    lis = list(map(lampda x: replace if x == search else x, my_list))
+    lis = list(map(lambda x: replace if x == search else x, my_list))
     return lis
