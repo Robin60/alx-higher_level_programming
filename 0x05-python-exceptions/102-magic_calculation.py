62cdca265c07e68ab2b909c29271e06532a99773
Robin60 <owinorobin05@gmail.com>
2023-01-23 21:01:30 +0000

    feat: python exceptions

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    res = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise exception("Exception")
+            else:
+                res += a ** b / i
+        except:
+            res = a + b
+            break
+        return (res)
