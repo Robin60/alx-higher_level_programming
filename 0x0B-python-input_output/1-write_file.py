f61ad840cd3b63eee1a7ed651020e5bcca77e71f
Robin Owino <owinorobin05@gmail.com>
2023-02-07 12:06:14 -0800

    feat: Python input output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -12,5 +12,5 @@ def write_file(filename="", text=""):
 
     """
     with open(filename, 'w', encoding='utf-8') as file:
-        chars_writted = file.write(text)
+        chars_written = file.write(text)
         return chars_written
