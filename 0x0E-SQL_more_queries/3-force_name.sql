3ada0bb5c5908c94e26ce51e243104deed75ffd2
Robin Owino <owinorobin05@gmail.com>
2023-03-15 07:07:18 -0700

    feat: Sql more queries

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
--- 0x0E. SQL - More queries, task 3. Always a name
--- Creates table `force_name` with `id and `name` fields. DB in args.
+-- 0x0E. SQL - More queries, Always a name
+-- Creates table `force_name` with `id and `name` fields.
 CREATE TABLE IF NOT EXISTS force_name (
        id INT,
        name VARCHAR(256) NOT NULL
