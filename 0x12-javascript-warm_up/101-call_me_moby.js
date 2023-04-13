5f3879b3f759a5ed4ab09bd4384e4cdee5e03297
Robin60 <owinorobin05@gmail.com>
2023-04-13 18:19:44 +0000

    fix: hot fix

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Executes x times a function
 
-exports.callMemoby = function (x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
   }
