f2b3d0f508a335206796ef28fc2a6d5c6374dca1
Robin60 <owinorobin05@gmail.com>
2023-04-17 13:10:28 +0000

    feat: Python ORM

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ import MySQLdb
 
 if __name__ == "__main__":
     con = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                        passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+                          passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
     cursor = con.cursor()
     cursor.execute("SELECT c.id, c.name, s.name FROM cities as c LEFT JOIN \
             states as s ON c.state_id = s.id;")
